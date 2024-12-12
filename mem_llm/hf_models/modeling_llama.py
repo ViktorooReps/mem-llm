e68ab0a009b2369dedba6d10e6d04c567ee749b9
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2024-12-12 14:45:09 +0100

    conversion script bugfix

--- mem_llm/hf_models/modeling_llama.py
+++ mem_llm/hf_models/modeling_llama.py
@@ -65,10 +65,10 @@ _CONFIG_FOR_DOC = "LlamaConfig"
 
 def download_llama(model_name: str, models_dir: Path | str = 'models'):
     from transformers.models.llama import LlamaForCausalLM as HfLlama
-    from transformers.models.llama import LlamaTokenizer as HfLlamaTokenizer
+    from transformers import AutoTokenizer
 
     HfLlama.from_pretrained(model_name).save_pretrained(Path(models_dir) / model_name)
-    HfLlamaTokenizer.from_pretrained(model_name).save_pretrained(Path(models_dir) / model_name)
+    AutoTokenizer.from_pretrained(model_name).save_pretrained(Path(models_dir) / model_name)
 
 
 def convert_llama_checkpoint(checkpoint: str | Path, dest: str | Path):
