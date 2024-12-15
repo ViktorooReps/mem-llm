53b9c6b6c3930c2cb6997f3f5002202096e96acb
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2024-12-15 16:40:30 +0100

    add arg sanitize

--- submit.sh
+++ submit.sh
@@ -4,6 +4,7 @@ source ~/.zshrc
 source set_env.sh
 cd dl-char-llm
 conda activate char-llm
+sanitized_arg=$(echo "$1" | tr '/' '_')
 PYTHONPATH='/mloscratch/homes/shcherba/dl-char-llm' torchrun --nproc_per_node=1 train.py \
     "$@" \
-    > /mloscratch/homes/shcherba/output_$1.log 2>&1
+    > /mloscratch/homes/shcherba/output_${sanitized_arg}.log 2>&1
