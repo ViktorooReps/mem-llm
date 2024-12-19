d63d961f002430595f36ad9024da6ed66df671a3
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2024-12-19 20:33:01 +0100

    added pg19 dataset loader

--- mem_llm/dataset.py
+++ mem_llm/dataset.py
@@ -2,7 +2,7 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from dataclasses import dataclass, is_dataclass
 from pathlib import Path
 from threading import Lock
-from typing import TypeVar
+from typing import TypeVar, Callable
 
 import numpy as np
 import requests
@@ -189,14 +189,14 @@ class FineWebEduConfig(DatasetConfig):
 def get_write_location(
         last_location: list[int],
         token_length: int,
-        max_end_location: int,
+        max_end_location: int | None,
         lock: Lock
 ) -> (int, int):
 
     with lock:
         start_location = last_location[0]
         last_location[0] += token_length
-        last_location[0] = min(last_location[0], max_end_location)
+        last_location[0] = min(last_location[0], max_end_location) if max_end_location is not None else last_location[0]
         return start_location, last_location[0]  # [start, end)
 
 
@@ -205,7 +205,7 @@ def tokenize_and_write(
         tokenizer: Tokenizer,
         arr: np.memmap,
         last_location: list[int],
-        max_location: int,
+        max_location: int | None,
         lock: Lock,
         source_dtype: np.dtype
 ):
@@ -336,6 +336,141 @@ def load_dataset_fwe(
     # Flush changes and close memmap
     del train_memmap
     del val_memmap
+
+    return (
+        GuaranteedLengthDataset(train_path, **extras, limit_dataset_length=config.train_length),
+        GuaranteedLengthDataset(val_path, **extras, limit_dataset_length=config.val_length),
+    )
+
+
+@dataclass
+class PG19Config(DatasetConfig):
+    num_download_workers: int = 12
+    dataset_name: str = 'pg19'
+
+
+def load_dataset_pg19(
+        config: FineWebEduConfig,
+        tokenizer: Tokenizer
+) -> (GuaranteedLengthDataset, GuaranteedLengthDataset):
+
+    logger.info(f"Loading {config.dataset_name} for {tokenizer.descriptor} tokenizer")
+
+    source_dtype = get_storage_type(tokenizer)
+
+    extras = {
+        'source_dtype': source_dtype,
+        'example_length': config.example_length,
+        'tile_size': config.tile_size,
+    }
+
+    cache_path = config.get_cache_path(tokenizer)
+
+    if cache_path.exists():
+        logger.info(f"Found {cache_path}, loading the dataset from there")
+        return (
+            GuaranteedLengthDataset(cache_path / 'train', **extras, limit_dataset_length=config.train_length),
+            GuaranteedLengthDataset(cache_path / 'val', **extras, limit_dataset_length=config.val_length),
+        )
+
+    dest = cache_path
+    dest.mkdir(parents=True, exist_ok=True)
+
+    # Paths for train and validation memmap files
+    train_path = dest / "train"
+    train_path.mkdir(exist_ok=True)
+    train_file = train_path / "tokens.dat"
+
+    val_path = dest / "val"
+    val_path.mkdir(exist_ok=True)
+    val_file = val_path / "tokens.dat"
+
+    test_file = dest / "test"
+    test_file.mkdir(exist_ok=True)
+    test_file = test_file / "tokens.dat"
+
+    train_max_size = 10_000_000_000
+    val_max_size = 10_000_000
+    test_max_size = 10_000_000
+
+    # Pre-allocate memmap files
+    train_memmap = create_token_memmap(train_file, (train_max_size,), source_dtype)
+    val_memmap = create_token_memmap(val_file, (val_max_size,), source_dtype)
+    test_memmap = create_token_memmap(val_file, (test_max_size,), source_dtype)
+
+    # Initialize shared indices and locks
+    train_last_location = [0]  # Shared variable to track train memmap location
+    val_last_location = [0]    # Shared variable to track val memmap location
+    test_last_location = [0]
+
+    train_lock = Lock()
+    val_lock = Lock()
+    test_lock = Lock()
+
+    def get_loader(split: str) -> DataLoader:
+        return DataLoader(load_dataset_from_hub(
+            'deepmind/pg19',
+            streaming=True,
+            split=split,
+            download_mode=DownloadMode.FORCE_REDOWNLOAD,
+            columns=['text'],
+            revision='4d28bd77e66947ad3835cf78ed7aaeb4dd87ad8b'
+        ), num_workers=config.num_download_workers, prefetch_factor=8, batch_size=32)
+
+    def get_worker(
+            dest_memmap: np.memmap,
+            last_location: list,
+            max_size: int,
+            lock: Lock,
+    ) -> Callable[[dict], None]:
+        def process_example(example) -> int | None:
+            return tokenize_and_write(example, tokenizer, dest_memmap, last_location, max_size, lock, source_dtype)
+
+        return process_example
+
+    def download(worker: Callable[[dict], None], loader: DataLoader) -> None:
+        with ThreadPoolExecutor(max_workers=config.num_download_workers) as executor:
+            for batch in HumanizedTqdm(loader, unit='books'):
+                futures = []
+
+                for example in batch['text']:
+                    future = executor.submit(worker, example)
+                    futures.append(future)
+
+                for future in futures:
+                    future.result()
+
+    download(
+        get_worker(train_memmap, train_last_location, train_max_size, train_lock),
+        get_loader('train')
+    )
+
+    download(
+        get_worker(val_memmap, val_last_location, val_max_size, val_lock),
+        get_loader('validation')
+    )
+
+    download(
+        get_worker(test_memmap, test_last_location, test_max_size, test_lock),
+        get_loader('test')
+    )
+
+    logger.info(f"Total train tokens written: {train_last_location[0]}")
+    logger.info(f"Total validation tokens written: {val_last_location[0]}")
+    logger.info(f"Total test tokens written: {test_last_location[0]}")
+
+    # Truncate the files to match the actual data size
+    with open(train_memmap.filename, "r+b") as f:
+        f.truncate(train_last_location[0] * train_memmap.dtype.itemsize)
+    with open(val_memmap.filename, "r+b") as f:
+        f.truncate(val_last_location[0] * val_memmap.dtype.itemsize)
+    with open(test_memmap.filename, "r+b") as f:
+        f.truncate(test_last_location[0] * test_memmap.dtype.itemsize)
+
+    # Flush changes and close memmap
+    del train_memmap
+    del val_memmap
+    del test_memmap
 
     return (
         GuaranteedLengthDataset(train_path, **extras, limit_dataset_length=config.train_length),
